11ebd6bc2b1d1b989c8ee0832fa0ffcc11b4e0fd
Kyle Bruxvoort <kbruxvoort@gmail.com>
2023-04-26 17:17:20 -0500

    add function for hidden parameter schema

--- lib/extensible_storage.py
+++ lib/extensible_storage.py
@@ -6,26 +6,29 @@ import Autodesk.Revit.DB.ExtensibleStorage as es
 HIDDEN_SCHEMA_GUID = Guid("f5f3b8c9-715b-40e3-a085-c5c3824d36f1")
 FETCH_SCHEMA_GUID = Guid("96a21166-5dcd-43ef-931b-958cff33df64")
 
+class ExtensibleSchema:
+    def __init__(self, guid=None):
+        self.guid = guid
+        
+    @property
+    def schema(self):
+        pass
 
-def create_schema():
-    schema_builder = es.SchemaBuilder(Guid("149f5c9f-45fb-4d6c-a98c-83f6478e9315"))
-    schema_builder.SetReadAccessLevel(AccessLevel.Public)
-    schema_builder.SetWriteAccessLevel(AccessLevel.Public)
-    schema_builder.SetVendorId("kbrux")
-    schema_builder.SetSchemaName("ParamTest")
-    schema_builder.SetDocumentation("Important documentation from Kyle")
-
-    schema_builder.AddSimpleField("HiddenParameters", str)
-    # schema_builder.AddSimpleField("Family", str)
-    # schema_builder.AddSimpleField("Type", str)
-    # schema_builder.AddSimpleField("Instance", str)
-    
-
-    schema = schema_builder.Finish()
+def create_hidden_param_schema():
+    builder = es.SchemaBuilder(HIDDEN_SCHEMA_GUID)
+    builder.SetReadAccessLevel(es.AccessLevel.Public)
+    builder.SetWriteAccessLevel(es.AccessLevel.Public)
+    builder.SetVendorId("pySSG")
+    builder.SetSchemaName("HiddenParameters")
+    builder.SetDocumentation(
+        "This schema is used to map visible family parameters to a shared hidden parameter."
+    )
+    field_builder = builder.AddSimpleField("data", str)
+    schema = builder.Finish()
     return schema
 
-def add_schema_entity(schema, element, value):
-    entity = Entity(schema)
-    field = schema.GetField("HiddenParameters")
+def add_schema_entity(schema, element, field_name, value):
+    entity = es.Entity(schema)
+    field = schema.GetField(field_name)
     entity.Set(field, value)
     element.SetEntity(entity)
