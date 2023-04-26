9499c798368ff8fd4f028db37be6f6bb907737c1
Kyle Bruxvoort <kbruxvoort@gmail.com>
2023-04-26 17:07:53 -0500

    refactor family parameters

--- lib/parameters/__init__.py
+++ lib/parameters/__init__.py
@@ -1,2 +1,17 @@
 from .family import *
 from .shared import *
+
+
+def get_array_original_element_ids(family_document):
+    arrays = DB.FilteredElementCollector(family_document).OfCategory(DB.BuiltInCategory.OST_IOSArrays).ToElements()
+    array_element_ids = []
+    for a in arrays:
+        array_element_ids.extend(a.GetOriginalMemberIds())
+    return array_element_ids
+
+def param_associated_to_array(parameter, array_element_ids):
+    if parameter.AssociatedParameters:
+        for ap in parameter.AssociatedParameters:
+            if ap.Element.GroupId in array_element_ids:
+                return True
+    return False
--- lib/parameters/family.py
+++ lib/parameters/family.py
@@ -1,6 +1,7 @@
 from Autodesk.Revit.Exceptions import InvalidOperationException, ArgumentException
 
-from pyrevit import revit, DB
+from pyrevit import revit, DB, HOST_APP
+
 
 
 PARAM_MAP = {
@@ -36,48 +37,110 @@ CATEGORY_MAP = {
         'Thickness': 3,
         'Height': 4,
     }
+        
 
-def get_value(fam_type, param):
-    value = ""
-    if param.StorageType == DB.StorageType.Double:
-        value = fam_type.AsDouble(param)
-        if value == None:
-            value = 0
-    elif param.StorageType == DB.StorageType.Integer:
-        value = fam_type.AsInteger(param)
-        if value == None:
-            value = 0
-    elif param.StorageType == DB.StorageType.String:
-        value = fam_type.AsString(param)
-        if value == None:
-            value = ""
-    elif param.StorageType == DB.StorageType.ElementId:
-        id_ = fam_type.AsElementId(param)
-        e = revit.doc.GetElement(id_)
-        if e:
-            value = e.Name
-    return value
+def is_built_in(family_parameter):
+    return family_parameter.definition.BuiltInParameter != DB.BuiltInParameter.INVALID
+    
 
-
-def param_has_value(fam_type, parameter):
-    if parameter.IsDeterminedByFormula:
-        return True
-    if parameter.Definition.ParameterType == DB.ParameterType.Text:
-        return bool(get_value(fam_type, parameter))
+def get_parameter_type_name(family_parameter):
+    if HOST_APP.is_newer_than(2022, or_equal=True):
+        return DB.LabelUtils.GetLabelForSpec(family_parameter.Definition.GetDataType())
     else:
-        return fam_type.HasValue(parameter)
+        return family_parameter.Definition.ParameterType.ToString()
     
-    
-def split_name(param_name):
-    name_split = param_name.split("_")
+
+def get_name(family_parameter):
+    return family_parameter.Definition.Name
+
+          
+def get_value(family_parameter, family_type):
+    if family_parameter.StorageType == DB.StorageType.Double:
+        return family_type.AsDouble(family_parameter)
+
+    elif family_parameter.StorageType == DB.StorageType.Integer:
+        return family_type.AsInteger(family_parameter)
+
+    elif family_parameter.StorageType == DB.StorageType.String:
+        return family_type.AsString(family_parameter)
+
+    elif family_parameter.StorageType == DB.StorageType.ElementId:
+        id_ = family_type.AsElementId(family_parameter)
+        return revit.doc.GetElement(id_)
+        
+
+def split_name(family_parameter):
+    name_split = family_parameter.Definition.Name.split("_")
     prefix = None
     if len(name_split) > 1:
         prefix = name_split[0].strip()
         suffix = " ".join(name_split[1:]).strip()
     else:
-        suffix = param_name
+        suffix = family_parameter.Definition.Name
     return (prefix, suffix)
     
+
+def has_value(family_parameter, family_type):
+    if family_parameter.IsDeterminedByFormula:
+        return True
+    if HOST_APP.is_newer_than(2022, or_equal=True):
+        is_text = family_parameter.Definition.GetDataType() == DB.SpecTypeId.String
+    else:
+        is_text = family_parameter.Definition.ParameterType == DB.ParameterType.Text
+    if is_text:
+        return bool(family_parameter.get_value(family_type))
+    else:
+        return family_type.HasValue(family_parameter)
+        
+
+def modify_group(family_parameter, new_group):
+    if not is_built_in(family_parameter):
+        family_parameter.Definition.ParameterGroup = new_group
+            
+
+def get_sorted_group(family_parameter, param_map_dict):
+    if not is_built_in(family_parameter):
+        if family_parameter.Definition.Visible is False:
+            return DB.BuiltInParameterGroup.INVALID
+        prefix, suffix = split_name(family_parameter)
+        if prefix:
+            if prefix.upper() == "ENTER" and suffix.lower() not in param_map_dict:
+                return DB.BuiltInParameterGroup.PG_CONSTRUCTION
+            for k,v in param_map_dict.items():
+                if k.upper() == prefix.upper():
+                    return v
+        if suffix:
+            for k,v in param_map_dict.items():
+                if k.lower() in suffix.lower():
+                    return v
+    return family_parameter.Definition.ParameterGroup
+    
+
+def param_associated_to_array(family_parameter, array_element_ids):
+    if family_parameter.AssociatedParameters:
+        for ap in family_parameter.AssociatedParameters:
+            if ap.Element.GroupId in array_element_ids:
+                return True
+    return False
+    
+
+def get_array_original_element_ids(family_document):
+    arrays = DB.FilteredElementCollector(family_document).OfCategory(DB.BuiltInCategory.OST_IOSArrays).ToElements()
+    array_element_ids = []
+    for a in arrays:
+        array_element_ids.extend(a.GetOriginalMemberIds())
+    return array_element_ids        
+
+
+def save_param_order(family_document):
+    fam_mgr = family_document.FamilyManager
+    sort_index = 0
+    param_dict = {}
+    for p in fam_mgr.GetParameters():
+        param_dict[p.Definition.Name] = sort_index
+        sort_index += 1
+    return param_dict
+
 
 def sort_parameter_into_group(parameter, param_map_dict):
     if parameter.UserModifiable is False:
@@ -98,27 +161,18 @@ def sort_parameter_into_group(parameter, param_map_dict):
                 return v
     return parameter.Definition.ParameterGroup
 
-
-def reorder_fetch(parameters):
-    pass
-
-def modify_parameter_group(parameter, parameter_group):
-    try:
-        parameter.Definition.ParameterGroup = parameter_group
-    except InvalidOperationException as ie:
-        print('"{}" is a builtin parameter and its group cannot be set'.format(parameter.Definition.Name))
         
-def replace_with_shared(fam_param, shared_param):
-    if fam_param.Definition.Name == shared_param.Name:
+def replace_with_shared(family_parameter, external_definition):
+    if family_parameter.Definition.Name == external_definition.Name:
         revit.doc.FamilyManager.RenameParameter(
-            fam_param, fam_param.Definition.Name + "_Temp"
+            family_parameter, family_parameter.Definition.Name + "_Temp"
         )
     try:
         return revit.doc.FamilyManager.ReplaceParameter(
-            fam_param,
-            shared_param,
-            fam_param.Definition.ParameterGroup,
-            fam_param.IsInstance,
+            family_parameter,
+            external_definition,
+            family_parameter.Definition.ParameterGroup,
+            family_parameter.IsInstance,
         )
 
     except InvalidOperationException as ie:
