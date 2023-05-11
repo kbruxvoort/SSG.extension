5b686ff85602f6e467489a4d54b527a8015ce021
Kyle Bruxvoort <kbruxvoort@gmail.com>
2023-05-11 11:09:10 -0500

    updated packing scripts

--- lib/packing.py
+++ lib/packing.py
@@ -1,74 +1,87 @@
-from pyrevit import revit, DB
+from pyrevit import DB
 
-def sort_views(views, view_type_order):
-    def key_function(view):
-        view_type_index = view_type_order.index(view.ViewType) if view.ViewType in view_type_order else len(view_type_order)
-        width = view.Outline.Max.U - view.Outline.Min.U
-        height = view.Outline.Max.V - view.Outline.Min.V
-        area = width * height
-        return (view_type_index, -area)
-    return sorted(views, key=key_function)
 
-        
-# def pack_views_to_sheet(views, sheet, start_point=(0, 0), spacing=1.0):
-#     x, y = start_point
-#     sheet_width = sheet.get_Parameter(DB.BuiltInParameter.SHEET_WIDTH).AsDouble()
-#     sheet_height = sheet.get_Parameter(DB.BuiltInParameter.SHEET_Height).AsDouble()
-#     max_height = 0
-    
-#     for view in views:
-#         view_width, view_height = view.Outline.Max - view.Outline.Min
-        
-#         # check if view fits on current sheet, otherwise create new sheet
-#         if x + view_width > sheet_width:
-#             x = start_point[0]
-#             y += max_height + spacing
-#             max_height = 0
-#             if y + view_height > sheet_height:
-#                 raise ValueError("Views do not fit on sheet")
-        
-#         # add view to sheet
-#         location_point = DB.XYZ(x, y, 0)
-#         view_scale = (sheet_height / view_height) * 96
-#         sheet.AddView(view, location_point, view_scale)
-        
-#         x += view_width + spacing
-#         max_height = max(max_height, view_height)
-        
-def pack_views_on_sheet(views, sheet, border=(.25, 2.25, .25, .25)):
-    top, right, bottom, left = (x/12 for x in border)
-    sheet_width = sheet.Outline.Max.U - sheet.Outline.Min.U
-    sheet_height = sheet.Outline.Max.V - sheet.Outline.Min.V
-    usable_width = sheet_width - right - left
-    usable_height = sheet_height - top - bottom
-    
-    x = left
-    y = bottom
-    # max_row_height = 0
-    viewports = []
-    title_offset = .85/12
-    max_height = max(view.Outline.Max.V - view.Outline.Min.V + title_offset for view in views)
-    
-    with revit.Transaction("Pack Views"):
-        for view in views:
-            view_width = view.Outline.Max.U - view.Outline.Min.U
-            view_height = view.Outline.Max.V - view.Outline.Min.V
-            
-            if x + view_width > usable_width:
-                # print("too wide")
-                x = left
-                y += max_height
-            
-            if y + view_height > usable_height:
-                raise ValueError("Not all views can fit in the sheet")
-            
-            vp = DB.Viewport.Create(
-                revit.doc, 
-                sheet.Id, 
-                view.Id, 
-                DB.XYZ(x + view_width/2 + title_offset, y + view_height/2 + title_offset, 0)
-            )
-            x += view_width
-            viewports.append(vp)
-        return viewports
+class Rectangle:
+    def __init__(self, view, width, height, x=0, y=0):
+        self.view = view
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
 
+
+class Bin:
+    def __init__(self, sheet, border_top=0, border_right=0, border_bottom=0, border_left=0):
+        self.sheet = sheet
+        self.border_top = border_top
+        self.border_right = border_right
+        self.border_bottom = border_bottom
+        self.border_left = border_left
+        self.x = border_left
+        self.y = border_bottom
+        self.width = self.sheet.Outline.Max.U - self.sheet.Outline.Min.U - self.border_left - self.border_right
+        self.height = self.sheet.Outline.Max.V - self.sheet.Outline.Min.V - self.border_top - self.border_bottom
+        self.remaining_areas = [Rectangle(None, self.width, self.height, self.border_left, self.border_bottom)]
+
+
+def find_best_fit(bin, rectangle):
+    best_fit_index = -1
+    best_fit_area = float('inf')
+
+    for i, remaining_area in enumerate(bin.remaining_areas):
+        if remaining_area.width >= rectangle.width and remaining_area.height >= rectangle.height:
+            remaining_area = remaining_area.width * remaining_area.height
+            if remaining_area < best_fit_area:
+                best_fit_area = remaining_area
+                best_fit_index = i
+
+    return best_fit_index
+
+def split_rectangle(rectangle, width, height):
+    remaining_areas = []
+    if width < rectangle.width:
+        remaining_areas.append(Rectangle(None, rectangle.width - width, rectangle.height, rectangle.x + width, rectangle.y))
+    if height < rectangle.height:
+        remaining_areas.append(Rectangle(None, width, rectangle.height - height, rectangle.x, rectangle.y + height))
+    return remaining_areas
+
+
+def pack_views(doc, view_rectangles, titleblock, border_top=0, border_right=0, border_bottom=0, border_left=0):
+    sheet = DB.ViewSheet.Create(doc, titleblock)
+    start_bin = Bin(sheet, border_top, border_right, border_bottom, border_left)
+    sheets = [start_bin]
+    
+    for view_rect in view_rectangles:
+        placed = False
+        
+        for sheet in sheets:
+            best_fit_index = find_best_fit(sheet, view_rect)
+            if best_fit_index != -1:
+                remaining_area = sheet.remaining_areas.pop(best_fit_index)
+                sheet.remaining_areas.extend(split_rectangle(remaining_area, view_rect.width, view_rect.height))
+                view_rect.x = remaining_area.x + view_rect.width / 2
+                view_rect.y = remaining_area.y + view_rect.height / 2
+                x_offset = sheet.border_left
+                y_offset = sheet.border_bottom
+                placement_pt = DB.XYZ(view_rect.x + x_offset, view_rect.y + y_offset, 0)
+                # print(view_rect.view.Name)
+                # print(view_rect.width, view_rect.height)
+                # print(placement_pt)
+                # print("---------")
+                DB.Viewport.Create(doc, sheet.sheet.Id, view_rect.view.Id, placement_pt)
+                placed = True
+                break
+            
+        if not placed:
+            new_sheet = DB.ViewSheet.Create(doc, titleblock)
+            new_sheet_bin = Bin(new_sheet, border_top, border_right, border_bottom, border_left)
+            remaining_area = new_sheet_bin.remaining_areas.pop(0)
+            new_sheet_bin.remaining_areas = split_rectangle(remaining_area, view_rect.width, view_rect.height)
+            view_rect.x = remaining_area.x + view_rect.width / 2
+            view_rect.y = remaining_area.y + view_rect.height / 2
+            x_offset = sheet.border_left
+            y_offset = sheet.border_bottom
+            DB.Viewport.Create(doc, new_sheet_bin.sheet.Id, view_rect.view.Id, DB.XYZ(view_rect.x + x_offset, view_rect.y + y_offset, 0))
+            sheets.append(new_sheet_bin)
+            
+    return sheets
--- pySSG.tab/Project Planning.Panel/Sheets.pushbutton/script.py
+++ pySSG.tab/Project Planning.Panel/Sheets.pushbutton/script.py
@@ -1,179 +1,75 @@
 import math
 
 from pyrevit import revit, script, forms, DB
-from packing import sort_views, pack_views_on_sheet
+from packing import *
+from rooms import select_placed_rooms
 
 
-# border_other = DB.UnitUtils.ConvertToInternalUnits(.25, DB.DisplayUnitType.DUT_DECIMAL_INCHES)
-# border_right = DB.UnitUtils.ConvertToInternalUnits(2.25, DB.DisplayUnitType.DUT_DECIMAL_INCHES)
-border_other = .25
-border_right = .25
-logger = script.get_logger()
+def map_room_views(rooms, views, placed_views):
+    grouped_views = {}
+    for room in rooms:
+        room_views = []
+        for view in views:
+            if not view.IsTemplate and room.Number in view.Name and view.Id not in placed_views:
+                room_views.append(view)
+            if room_views:    
+                grouped_views[room.Number] = room_views
+    return grouped_views
 
+def is_view_valid(view_rect, sheet_width, sheet_height):
+    if view_rect.width > sheet_width or view_rect.height > sheet_height:
+        return False
+    return True
+
+
+
+# BORDERS = {'top': .1, 'right': .25, 'bottom': .1, 'left': .1}    
 
 selected_titleblock = forms.select_titleblocks()
 if selected_titleblock:
-    area_param_id = DB.ElementId(DB.BuiltInParameter.ROOM_AREA)
-    area_param_prov = DB.ParameterValueProvider(area_param_id)
-    param_greater = DB.FilterNumericGreater()
-    filt = DB.ElementParameterFilter(
-        DB.FilterDoubleRule(area_param_prov, param_greater, 0, 1e-6)
-    )
 
-    rooms = DB.FilteredElementCollector(revit.doc).WherePasses(filt).ToElements()
+    rooms = select_placed_rooms(revit.doc)
     sheets = DB.FilteredElementCollector(revit.doc).OfCategory(DB.BuiltInCategory.OST_Sheets)
     placed_views = []
     for sheet in sheets:
         placed_views.extend(sheet.GetAllPlacedViews())
-
-    views = []
-    keys = []
-    groups = []
 
     views = (
         DB.FilteredElementCollector(revit.doc)
         .OfCategory(DB.BuiltInCategory.OST_Views)
         .ToElements()
     )
-    grouped_views = {}
-    
-    for room in rooms:
-        room_num = room.Number
-        room_views = []
-        room_sheets = []
-        for view in views:
-            if not view.IsTemplate and room_num in view.Name and view.Id not in placed_views:
-                # and view.get_Parameter("View Organization").AsString() == "FINAL DRAWINGS"
-                room_views.append(view)
-        if room_views:    
-            grouped_views[room_num] = {"unplaced_views": room_views}
-        for sheet in sheets:
-            if room_num in sheet.get_Parameter(DB.BuiltInParameter.SHEET_NAME).AsString():
-                room_sheets.append(sheet)
-        if room_sheets:
-            grouped_views[room_num] = {"existing_sheets": room_sheets}
-            # vName = view.Name.split("-")[1].strip()
-            # data.append(vName)
-    # sort_order = [
-    #     "ThreeD",
-    #     "Elevation"
-    #     "FloorPlan", 
-    #     "Section"
-        
-    # ]
+
     sort_order = [
-        DB.ViewType.ThreeD,
         DB.ViewType.FloorPlan,
-        DB.ViewType.Elevation, 
-        DB.ViewType.Section, 
-
+        DB.ViewType.Elevation,
+        DB.ViewType.ThreeD, 
+        DB.ViewType.Section,
+        # add schedule 
     ]
-    for k,v in grouped_views.items():
-        # view_widths = [view.Outline.Max.U - view.Outline.Min.U for view in v]
-        # view_heights = [view.Outline.Max.V - view.Outline.Min.V for view in v]
-        # print("Room {}".format(k))
-        # print("\tWidths: {}".format(view_widths))
-        # print("\tHeights: {}".format(view_heights))
-        print(k, v)
-    '''
-    views = sort_views(views, sort_order)
-    
-    for v in views:
-        view_type_index = sort_order.index(v.ViewType) if v.ViewType in sort_order else len(sort_order)
-        width = v.Outline.Max.U - v.Outline.Min.U
-        height = v.Outline.Max.V - v.Outline.Min.V
-        area = width * height
-        # print(v.Name, v.ViewType, area, view_type_index)
-        
-    sheet_id = 957271
-    sheet = revit.doc.GetElement(DB.ElementId(sheet_id))
-    # print(sheet)
 
-    pack_views_on_sheet(views, sheet)
-    # sorted_views = []
-    # for x in sort_order:
-    #     for view in views:
-    #         if str(view.ViewType) == x:
-    #             sorted_views.append(view)
+    grouped_views = map_room_views(rooms, views, placed_views)
+    title_offset_x=.25/12
+    # title_offset_x=1.0/12
+    title_offset_y=1.1/12
+    with revit.Transaction("Pack views"):
+        for number, views in grouped_views.items():
 
-    # keyfunc = lambda x: x.Name.split("-")[1].strip()
-    # sorted_views.sort(key=keyfunc)
-    # for k, g in groupby(sorted_views, keyfunc):
-    #     keys.append(k)
-    #     groups.append(list(g))
-
-    # new_groups = []
-    # for group in groups:
-    #     view_num = len(group)
-    #     sheet_num = math.ceil(view_num / 4.0)
-
-    #     if view_num > 4:
-    #         new_groups.append(group[:4])
-    #         new_groups.append(group[4:])
-    #     else:
-    #         new_groups.append(group)
-    '''
-'''
-    currentSheets = []
-    col3 = (
-        DB.FilteredElementCollector(revit.doc)
-        .OfCategory(DB.BuiltInCategory.OST_Sheets)
-        .ToElements()
-    )
-    for s in col3:
-        try:
-            num = float(s.SheetNumber)
-            currentSheets.append(num)
-        except ValueError:
-            continue
-
-    if currentSheets:
-        max_num = math.floor(max(currentSheets))
-    else:
-        max_num = 0
-
-    # for v in views:
-    #     print(v.ViewType)
-
-    with revit.Transaction("Create Sheets and Pack Views"):
-        sheet_views = pack_views_on_sheets(
-            views,
-            revit.doc.GetElement(selected_titleblock),
-            sheet_name="103",
-            sheet_number=str(float(max_num+1)),
-            view_type_order=sort_order,
-            border=(border_other, border_right, border_other, border_other)
-        )
-#     sheet_list = []
-#     for idx, (room, group) in enumerate(zip(sorted(rooms), groups)):
-#         roomName = room.LookupParameter("Name").AsString()
-#         roomNumber = room.LookupParameter("Number").AsString()
-#         startNumber = max_num + 1 + idx
-#         view_num = len(group)
-#         sheet_num = int(math.ceil(view_num / 4.0))
-
-#         for i in range(0, sheet_num):
-#             newsheet = ViewSheet.Create(revit.doc, selected_titleblocks)
-#             newsheet.SheetNumber = str(startNumber + float(i) / 10)
-#             newsheet.Name = roomName + " - " + roomNumber
-#             print('Creating sheet "%s: %s"' % (newsheet.SheetNumber, newsheet.Name))
-#             sheet_list.append(newsheet)
-
-
-# with revit.Transaction("Add Views to Sheets"):
-
-#     for s, gp in zip(sheet_list, new_groups):
-#         locs = [top_left, top_right, bottom_left, bottom_right]
-#         locs_cycle = cycle(locs)
-#         for g in gp:
-#             l = next(locs_cycle)
-#             # print(l)
-#             vp = Viewport.Create(revit.doc, s.Id, g.Id, l)
-#             print('\tAdding "%s" to sheet "%s"' % (g.Name, s.Name))
-
-#     # for g in new_groups:
-#     #     print(g)
-
-
-# print("Completed\n")
-'''
+            view_rectangles = [Rectangle(
+                v, 
+                width=v.Outline.Max.U - v.Outline.Min.U + title_offset_x, 
+                height=v.Outline.Max.V - v.Outline.Min.V + title_offset_y
+            )
+                for v in views]
+            
+            view_rectangles.sort(key=lambda v: (v.height, v.width), reverse=True)
+            
+            sheets = pack_views(
+                revit.doc,
+                view_rectangles, 
+                selected_titleblock,  
+                border_top=.25/12, 
+                border_right=2.25/12, 
+                border_bottom=.25/12, 
+                border_left=.25/12,
+            )
